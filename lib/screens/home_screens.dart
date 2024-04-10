c177fd7661aff60c049c510e5f070c5beb8f0698
João Gabriel <jgmoura.dev@gmail.com>
2024-04-10 19:55:15 -0300

    finalização tela inicial

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -7,19 +7,84 @@ class HomeScreens extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gerador de senhas'),
-        centerTitle: true,
-      ),
-      body: Container(
-          padding: const EdgeInsets.all(20),
-          child: const Column(
-            children: [
-              CheckBoxList('Lower Case', false),
-              CheckBoxList('Upper case', false),
-              CheckBoxList('Números', false)
-            ],
-          )),
-    );
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'Gerador de senhas',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  const CheckBoxList('Lower Case', false),
+                  const CheckBoxList('Upper case', false),
+                  const CheckBoxList('Números', false),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const SizedBox(
+                          width: 100,
+                          child: TextField(
+                            decoration: InputDecoration(labelText: 'Tamanho'),
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Faz Alguma coisa');
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black),
+                          child: const Text(
+                            '+',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                      const SizedBox(
+                        width: 16,
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Faz outra coisa');
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black),
+                          child: const Text(
+                            '-',
+                            style: TextStyle(color: Colors.white),
+                          ))
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  SizedBox(
+                      width: 400,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black),
+                          onPressed: () {},
+                          child: const Text(
+                            'Gerar senha',
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                  SizedBox(
+                      width: 400,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black),
+                          onPressed: () {},
+                          child: const Text(
+                            'Copiar senha',
+                            style: TextStyle(color: Colors.white),
+                          ))),
+                  const TextField()
+                ],
+              )),
+        ));
   }
 }
